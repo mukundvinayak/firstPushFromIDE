3b7cf83cb5d4e9211ca986363fa672e4b146046c
Mukund Vinayak <mukund.vinayak3@gmail.com>
2023-01-02 22:22:32 +0530

    Hello first commit from intelliJ

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Person person = new Person("Mukund");
+        System.out.println(person.getName());
+    }
+}
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
